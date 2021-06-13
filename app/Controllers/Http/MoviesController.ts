80899da99447daa801ae5fa90a5cc78cb9d329cd
Marvin Parada <hello@marvin.dev>
2021-06-13 16:14:12 -0700

    NEW: Add validation to Movie [store] request.

--- app/Controllers/Http/MoviesController.ts
+++ app/Controllers/Http/MoviesController.ts
@@ -1,4 +1,5 @@
 import Movie from 'App/Models/Movie'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
 export default class MoviesController {
   public async index() {
@@ -6,15 +7,45 @@ export default class MoviesController {
     return movies
   }
 
-  public async store({ request }) {
-    await Movie.create({
-      purchasedAt: request.input('purchased_at'),
-      purchaseLocation: request.input('purchase_location'),
-      libraryID: request.input('library_id'),
-      notes: request.input('notes'),
-      info: request.input('info'),
-      format: request.input('format'),
+  public async store({ request, response }) {
+    const addToLibrarySchema = schema.create({
+      purchased_at: schema.date(),
+      purchase_location: schema.string(),
+      library_id: schema.string(),
+      notes: schema.string(),
+      info: schema.object().members({
+        title: schema.string(),
+        year: schema.number(),
+        genre: schema.array().members(schema.string()),
+        director: schema.array().members(schema.string()),
+        writer: schema.array().members(schema.string()),
+        actors: schema.array().members(schema.string()),
+        plot: schema.string(),
+        cover: schema.string(),
+        imdb_id: schema.string({}, [rules.unique({ table: 'movies', column: 'imdb_id' })]), // MOVE TO PARENT SCOPE
+        type: schema.enum(['movie', 'series']),
+      }),
+      format: schema.object().members({
+        bluray_hd: schema.boolean(),
+        bluray_uhd: schema.boolean(),
+        digital: schema.boolean(),
+        dvd: schema.boolean(),
+      }),
     })
+
+    try {
+      await request.validate({ schema: addToLibrarySchema })
+      await Movie.create({
+        purchasedAt: request.input('purchased_at'),
+        purchaseLocation: request.input('purchase_location'),
+        libraryID: request.input('library_id'),
+        notes: request.input('notes'),
+        info: request.input('info'),
+        format: request.input('format'),
+      })
+    } catch (error) {
+      response.badRequest(error.messages)
+    }
   }
 
   public async show({ params }) {
